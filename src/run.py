61ded993bbd9c1a2e55ef3000e250647a87f9088
Akinori Ito <ito@cssc.co.jp>
2023-06-05 22:56:24 +0900

    Google speadのメニュー表の更新

--- src/run.py
+++ src/run.py
@@ -1,4 +1,5 @@
 import argparse
+from datetime import date, datetime
 from menu_list import MenuList
 
 
@@ -9,7 +10,12 @@ def get_option() -> argparse.Namespace:
         argparse.Namespace: コマンドライン引数
     """
     argparser = argparse.ArgumentParser(
-        prog="run", usage="run.py -o operation", description="create/update menu list"
+        prog="run",
+        usage="run.py -d date -o operation",
+        description="create/update menu list",
+    )
+    argparser.add_argument(
+        "-d", "--date", type=str, help="date", default=date.today().isoformat()
     )
     argparser.add_argument(
         "-o", "--operation", type=str, help="operation", default="create"
@@ -22,12 +28,16 @@ def main():
     # 入力引数の取得
     args = get_option()
 
+    # 日付
+    this_date = datetime.strptime(args.date, "%Y-%m-%d").date()
+
     ml = MenuList()
     if args.operation == "create":
         # PDFのメニュー表からCSVのメニュー表の作成
-        ml.create_menu_csv()
+        ml.create_menu_csv(this_date)
     elif args.operation == "update":
-        ml.update_menu()
+        # Google sheetのメニュー表を更新
+        ml.update_menu_spreadsheet(this_date)
     else:
         print("Invalid operation")
 
